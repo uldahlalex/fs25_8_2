138befced716df44090a2e837fa856c19653a2fd
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-02-27 10:09:34 +0100

    path for backend

--- ExerciseCSolution/client/src/App.tsx
+++ ExerciseCSolution/client/src/App.tsx
@@ -5,7 +5,7 @@ import Admin from "./Admin.tsx";
 import Lobby from "./Lobby.tsx";
 
 export default function App() {
-    return (<WsClientProvider url={'ws://localhost:8080?id=' + crypto.randomUUID()}>
+    return (<WsClientProvider url={'wss://kahoot-267099996159.europe-north1.run.app?id=' + crypto.randomUUID()}>
             <Toaster />
             <Lobby />
             <Game />
