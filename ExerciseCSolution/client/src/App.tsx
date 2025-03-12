5bce462415bb6fe90b160934a620a45dbc0d020d
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-03-12 09:55:05 +0100

    updating url

--- ExerciseCSolution/client/src/App.tsx
+++ ExerciseCSolution/client/src/App.tsx
@@ -5,7 +5,7 @@ import Admin from "./Admin.tsx";
 import Lobby from "./Lobby.tsx";
 
 export default function App() {
-    return (<WsClientProvider url={'wss://kahoot-267099996159.europe-north1.run.app?id=' + crypto.randomUUID()}>
+    return (<WsClientProvider url={'https://kahootclone-267099996159.europe-north1.run.app?id=' + crypto.randomUUID()}>
             <Toaster />
             <Lobby />
             <Game />
